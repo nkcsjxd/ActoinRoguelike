dfa4c2ffc9dc8101885b1836ec118a3be304bbf5
nkcsjxd <nkcsjxd@proton.me>
2023-04-09 16:28:14 +0800

    first-project
    第一个空项目

---
+++ Source/ActoinRoguelike/ActoinRoguelikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActoinRoguelikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTOINROGUELIKE_API AActoinRoguelikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
